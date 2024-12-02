454156edb0522ce99f45aa7aeca8e629d93acfcc
pkroening <peter.kroening@eonerc.rwth-aachen.de>
2024-12-02 15:51:51 +0100

    remove multiindex

--- flexibility_quantification/modules/shadow_mpc.py
+++ flexibility_quantification/modules/shadow_mpc.py
@@ -45,8 +45,7 @@ class FlexibilityShadowMPC(mpc_full.MPC):
         if self.agent.config.id == inp.source.agent_id:
             return
 
-        # vals = strip_multi_index(inp.value)
-        vals = fill_nans(inp.value)
+        vals = strip_multi_index(inp.value)
 
         # the MPC Predictions starts at t=env.now not t=0
         vals.index += self.env.time
