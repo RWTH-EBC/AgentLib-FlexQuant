d5992460af24b235afb8745598bf389865f2bfae
felix.stegemerten <felix.stegemerten@eonerc.rwth-aachen.de>
2025-01-24 12:59:35 +0100

    Add setup for package installation

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+# Read requirements.txt
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setuptools.setup(
+    name="flexquant",
+    version="0.1.0",
+    author="",
+    author_email="",
+    description="Flexibility quantification setup based on agentlib_mpc",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=requirements
+)
