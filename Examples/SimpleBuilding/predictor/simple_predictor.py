f6e203c7641a3d2ff0e5b388674499e3ad94ff65
Suresh <fse-ksu@eonerc.rwth-aachen.de>
2025-02-14 18:19:53 +0100

    Minor comment changes

--- Examples/SimpleBuilding/predictor/simple_predictor.py
+++ Examples/SimpleBuilding/predictor/simple_predictor.py
@@ -5,7 +5,7 @@ from typing import List
 
 class PredictorModuleConfig(al.BaseModuleConfig):
     """Module that outputs a prediction of the ambient temp and comfort setpoint
-    at a specified interval."""
+    at a specified interval"""
 
     outputs: al.AgentVariables = [
         al.AgentVariable(
@@ -78,7 +78,7 @@ class PredictorModuleConfig(al.BaseModuleConfig):
 
 class PredictorModule(al.BaseModule):
     """Module that outputs a prediction of the ambient temp and comfort setpoint
-    at a specified interval."""
+    at a specified interval"""
 
     config: PredictorModuleConfig
 
@@ -86,21 +86,18 @@ class PredictorModule(al.BaseModule):
         pass
 
     def process(self):
-        """Sets a new prediction at each time step."""
+        """Sets a new prediction at each time step"""
         self.env.process(self.send_upper_comfort_trajectory())
         self.env.process(self.send_lower_comfort_trajectory())
         self.env.process(self.send_price_var_trajectory())
-
-        #TODO: why is this not in a function?
+        
         while True:
             ts = self.get("prediction_sampling_time").value
             n = self.get("prediction_horizon").value
             now = self.env.now
             sample_time = self.get("sampling_time").value
 
-            # temperature prediction
-            #TODO: check / confirm if vals make sense, grid makes sense
-            #TODO: change grid param n to n+1  
+            # temperature prediction   
             grid = np.arange(now, now + n * ts, ts)
             values = amb_temp_func(grid, uncertainty=0)
             traj = pd.Series(values, index=list(grid))
@@ -108,13 +105,12 @@ class PredictorModule(al.BaseModule):
             yield self.env.timeout(sample_time)
 
     def send_upper_comfort_trajectory(self):
-        """Sends the series for the comfort condition."""
+        """Sends the series for the comfort condition"""
         while True:
             now = self.env.now
             comfort_interval = self.get("comfort_interval").value
 
             # temperature prediction
-            #TODO: check / confirm if vals make sense, grid makes sense 
             grid = np.arange(now, now + 2 * comfort_interval, 0.5 * comfort_interval)
             values = [self.get("upper_comfort_high").value, self.get("upper_comfort_low").value] * 2
             traj = pd.Series(values, index=list(grid))
@@ -122,13 +118,12 @@ class PredictorModule(al.BaseModule):
             yield self.env.timeout(comfort_interval)
 
     def send_lower_comfort_trajectory(self):
-        """Sends the series for the comfort condition."""
+        """Sends the series for the comfort condition"""
         while True:
             now = self.env.now
             comfort_interval = self.get("comfort_interval").value
 
-            # temperature prediction
-            #TODO: check / confirm if vals make sense, grid makes sense 
+            # temperature prediction 
             grid = np.arange(now, now + 2 * comfort_interval, 0.5 * comfort_interval)
             values = [self.get("lower_comfort_low").value, self.get("lower_comfort_high").value] * 2
             traj = pd.Series(values, index=list(grid))
@@ -136,7 +131,7 @@ class PredictorModule(al.BaseModule):
             yield self.env.timeout(comfort_interval)
     
     def send_price_var_trajectory(self):
-        "Sends the series for the price variable"
+        """Sends the series for the price variable"""
         while True:
             ts = self.get("prediction_sampling_time").value
             n = self.get("prediction_horizon").value
@@ -149,7 +144,7 @@ class PredictorModule(al.BaseModule):
             yield self.env.timeout(sample_time)
 
 def amb_temp_func(current, uncertainty):
-    """Returns the ambient temperature in K, given a time in seconds."""
+    """Returns the ambient temperature in K, given a time in seconds"""
     value = np.zeros(shape=current.shape)
     for i in range(current.size):
         random_factor = 1 + uncertainty * (np.random.random() - 0.5)
