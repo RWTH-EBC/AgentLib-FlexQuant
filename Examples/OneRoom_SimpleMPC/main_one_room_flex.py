2fdaa2ce5d739d66357ee97b6b3d688df11b2107
Jason Klein <Jason.Klein@rwth-aachen.de>
2025-02-18 13:25:02 +0100

    Extended the automatic simulation for OneRooom_SimpleMPC

--- Examples/OneRoom_SimpleMPC/main_one_room_flex.py
+++ Examples/OneRoom_SimpleMPC/main_one_room_flex.py
@@ -93,12 +93,13 @@ if __name__ == "__main__":
         clear_files(bClear_plots=True, bClear_results=True, bClear_flex_files=True)
 
     offer_types: list[str] = ["neg", "pos", "real"]
+    # offer_types: list[str] = ["neg"]
     for offer_type in offer_types:
         print(f'\n{"":-^50}')
         print(f'{f" Starting simulation with {offer_type} ":-^50}')
         print(f'{"":-^50}\n')
 
-        # Edit market.json
+        # read market.json
         file_path: str = os.path.join("flex_configs", "market.json")
         with open(file_path, "r") as market_file:
             market_data = json.load(market_file)
@@ -115,7 +116,7 @@ if __name__ == "__main__":
         with open(file_path, "w") as market_file:
             json.dump(market_data, market_file, indent=4)
 
-        # Edit flexibility_agent_config.json
+        # read flexibility_agent_config.json
         file_path: str = os.path.join("flex_configs", "flexibility_agent_config.json")
         with open(file_path, "r") as flex_agent_config:
             flex_agent_data = json.load(flex_agent_config)
@@ -129,6 +130,36 @@ if __name__ == "__main__":
         # create new flexiblity_agent_config.json
         with open(file_path, "w") as flex_agent_config:
             json.dump(flex_agent_data, flex_agent_config, indent=4)
+
+        # read simple_model.json
+        file_path: str = os.path.join("mpc_and_sim", "simple_model.json")
+        with open(file_path, "r") as model_config:
+            model_config_data = json.load(model_config)
+            model_config.close()
+
+        model_config_data["modules"][1]["optimization_backend"]["results_file"] = f"results/mpc_{offer_type}.csv"
+
+        # delete old json file
+        os.remove(file_path)
+
+        # create new simple_model.json
+        with open(file_path, "w") as model_config:
+            json.dump(model_config_data, model_config, indent=4)
+
+        # read simple_sim.json
+        file_path: str = os.path.join("mpc_and_sim", "simple_sim.json")
+        with open(file_path, "r") as sim_config:
+            sim_config_data = json.load(sim_config)
+            sim_config.close()
+
+        sim_config_data["modules"][1]["result_filename"] = f"results/sim_room_{offer_type}.csv"
+
+        # delete old json file
+        os.remove(file_path)
+
+        # create new simple_sim.json
+        with open(file_path, "w") as sim_config:
+            json.dump(sim_config_data, sim_config, indent=4)
 
         run_example(until=until, offer_type=offer_type)
 
