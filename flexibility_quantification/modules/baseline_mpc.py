cf67a01fc2c2b288517f9a7039f8b9847dd0d10f
Hexuan <hexuan.zhao@rwth-aachen.de>
2025-05-07 21:14:28 +0200

    add module variables for controls full trajectory in BaselineMPC

--- flexibility_quantification/modules/baseline_mpc.py
+++ flexibility_quantification/modules/baseline_mpc.py
@@ -1,4 +1,23 @@
+from typing import List
+from pydantic import model_validator, Field
+from agentlib import AgentVariable
 from agentlib_mpc.modules import mpc_full, minlp_mpc
+from agentlib_mpc.data_structures.mpc_datamodels import Results, MPCVariable
+from flexibility_quantification.data_structures.globals import full_trajectory_suffix
+
+
+class FlexibilityBaselineMPCConfig(mpc_full.MPCConfig):
+
+    full_controls: List[AgentVariable] = Field(default=[])
+
+    @model_validator(mode='after')
+    def init_full_controls(cls, model):
+        '''fill the full_controls list if it's empty
+        '''
+        if not model.full_controls:
+            for control in model.controls:
+                model.full_controls.append(AgentVariable(name=control.name+full_trajectory_suffix, alias=control.name+full_trajectory_suffix))
+            return model
 
 
 class FlexibilityBaselineMPC(mpc_full.MPC):
@@ -14,6 +33,16 @@ class FlexibilityBaselineMPC(mpc_full.MPC):
             # For the end of the flex interval add time step!
             self.set("rel_end", self.get("_P_external").value.index[-1] -
                      self.env.time + timestep)
+
+    def set_actuation(self, solution: Results):
+        super().set_actuation(solution)
+        for full_control in self.config.full_controls:
+            # add full_control to the variables dictionary, so that the set function can be applied to it
+            self._variables_dict[full_control.name] = full_control
+            # get the corresponding control name
+            control = full_control.name.replace(full_trajectory_suffix, "")
+            # set value to full_control
+            self.set(full_control.name, solution.df.variable[control])
 
 
 class FlexibilityBaselineMINLPMPC(minlp_mpc.MINLPMPC):
