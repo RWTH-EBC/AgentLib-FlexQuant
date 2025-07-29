f784a64d418af872e530039986d8be4ed2e640cf
felix.stegemerten <felix.stegemerten@eonerc.rwth-aachen.de>
2025-07-29 19:09:17 +0200

    Fix pages doc

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,6 +9,9 @@ import sys
 from pathlib import Path
 
 sys.path.insert(0, os.path.abspath('../../'))
+sys.path.insert(0, os.path.abspath("."))
+sys.path.insert(0, os.path.abspath("../"))
+sys.setrecursionlimit(1500)
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
