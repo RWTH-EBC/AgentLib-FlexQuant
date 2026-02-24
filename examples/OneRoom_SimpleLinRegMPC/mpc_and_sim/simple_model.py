099df9fd56323f208e32b76b0b746bfecdd11e6b
sarahleidolf <sarah.leidolf@eonerc.rwth-aachen.de>
2026-02-24 20:26:31 +0100

    move example

---
+++ examples/OneRoom_SimpleLinRegMPC/mpc_and_sim/simple_model.py
@@ -0,0 +1,118 @@
+from agentlib_mpc.models.casadi_model import (
+    CasadiModel,
+    CasadiInput,
+    CasadiState,
+    CasadiParameter,
+    CasadiOutput,
+    CasadiModelConfig,
+)
+from typing import List
+from math import inf
+from agentlib_mpc.models.casadi_ml_model import CasadiMLModel, CasadiMLModelConfig
+
+
+class BaselineMPCModelConfig(CasadiMLModelConfig):
+    inputs: List[CasadiInput] = [
+        # controls
+        CasadiInput(
+            name="mDot", value=0.0225, unit="kg/s", description="Air mass flow into zone"
+        ),
+        # disturbances
+        CasadiInput(
+            name="load", value=150, unit="W", description="Heat " "load into zone"
+        ),
+        CasadiInput(
+            name="T_in", value=280.15, unit="K", description="Inflow air temperature"
+        ),
+        # settings
+        CasadiInput(
+            name="T_upper",
+            value=294.15,
+            unit="K",
+            description="Upper boundary (soft) for T.",
+        ),
+        CasadiInput(
+            name="T_lower",
+            value=292.15,
+            unit="K",
+            description="Upper boundary (soft) for T.",
+        ),
+    ]
+
+    states: List[CasadiState] = [
+        # differential
+        CasadiState(
+            name="T", value=293.15, unit="K", description="Temperature of zone"
+        ),
+        # algebraic
+        # slack variables
+        CasadiState(
+            name="T_slack",
+            value=0,
+            unit="K",
+            description="Slack variable of temperature of zone",
+        ),
+
+    ]
+    parameters: List[CasadiParameter] = [
+        CasadiParameter(
+            name="cp",
+            value=1000,
+            unit="J/kg*K",
+            description="thermal capacity of the air",
+        ),
+        CasadiParameter(
+            name="C", value=100000, unit="J/K", description="thermal capacity of zone"
+        ),
+        CasadiParameter(
+            name="s_T",
+            value=1,
+            unit="-",
+            description="Weight for T in constraint function",
+        ),
+        CasadiParameter(
+            name="r_mDot",
+            value=1,
+            unit="-",
+            description="Weight for mDot in objective function",
+        ),
+
+    ]
+    outputs: List[CasadiOutput] = [
+        CasadiOutput(name="T_out", unit="K", description="Temperature of zone"),
+        CasadiOutput(
+            name="P_el",
+            unit="W",
+            description="The power input to the system",
+        ),
+        CasadiOutput(name="Time", unit="s", description="Test casadi time")
+    ]
+
+class BaselineMPCModel(CasadiMLModel):
+    config: BaselineMPCModelConfig
+
+    def setup_system(self):
+        # Define ode
+        self.T_out.alg = self.T
+        self.P_el.alg = self.cp * self.mDot * (self.T - self.T_in) / 1000
+        self.Time.alg = self.time
+
+        # Constraints: List[(lower bound, function, upper bound)]
+        self.constraints = [
+            # soft constraints
+            (self.T_lower, self.T + self.T_slack, inf),
+            (-inf, self.T - self.T_slack, self.T_upper),
+            (0, self.T_slack, inf)
+        ]
+        # Objective function
+        objective = sum(
+            [
+                self.r_mDot * self.mDot,
+                self.s_T * self.T_slack ** 2,
+            ]
+        )
+        return objective
+
+
+
+
